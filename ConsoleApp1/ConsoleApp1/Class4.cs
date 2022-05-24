ff742a0138d0a729b0faac36a145440a87613d02
sonuchamlewad <csneha2620@gmail.com>
2022-05-24 12:59:07 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class4
+    {
+        static void Main(string[] args)
+        {
+            string s, revs = "";
+            Console.WriteLine(" Enter string");
+            s = Console.ReadLine();
+            for (int i = s.Length - 1; i >= 0; i--) 
+            {
+                revs += s[i].ToString();
+            }
+            if (revs == s) 
+            {
+                Console.WriteLine("String is Palindromen");
+            }
+            else
+            {
+                Console.WriteLine("String is not Palindrome");
+            }
+            Console.ReadLine();
+        }
+    }
+}
