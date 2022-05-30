e833f78e828ede6d75a35ad5429c0193409c696a
sonuchamlewad <csneha2620@gmail.com>
2022-05-30 10:09:57 +0530

    first commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
