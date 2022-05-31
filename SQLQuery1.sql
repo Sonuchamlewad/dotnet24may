994d6bc5de58352117c9a7e7898c7743ccb95c57
sonuchamlewad <csneha2620@gmail.com>
2022-05-31 16:04:33 +0530

    first commit

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,4 +6,9 @@ insert into sample values(1,'sneha1');
 insert into sample values(1,'sneha2');
 insert into sample values(1,'sneha3');
 insert into sample values(1,'sneha4')
+create table Employee(id int, empname varchar(200),gender varchar(10));
+insert into Employee values(1,'sonu','female');
+insert into Employee values(2,'sonu1','female');
+insert into Employee values(3,'Abc','male');
+select * from Employee;
 
