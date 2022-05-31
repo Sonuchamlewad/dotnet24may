11581f4357603379bab1ddb891710676a06d58d4
sonuchamlewad <csneha2620@gmail.com>
2022-05-31 15:51:34 +0530

    first commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+create database SampleDb2;
+Drop database SampleDb2;
+create table sample(id int,name varchar(200));
+select * from sample;
+insert into sample values(1,'sneha1');
+insert into sample values(1,'sneha2');
+insert into sample values(1,'sneha3');
+insert into sample values(1,'sneha4')
+
