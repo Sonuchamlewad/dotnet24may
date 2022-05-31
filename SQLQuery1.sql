3a3a96e4eb33d4b8ec2f7109d751accaf6c4f9cc
sonuchamlewad <csneha2620@gmail.com>
2022-05-31 16:56:41 +0530

    first commit

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -11,4 +11,40 @@ insert into Employee values(1,'sonu','female');
 insert into Employee values(2,'sonu1','female');
 insert into Employee values(3,'Abc','male');
 select * from Employee;
+create table student(id int,rollno int,FirstName varchar(20),LastName varchar(20),Gender varchar(10));
+
+select * from student;
+
+
+insert into student values(1,'101','sangam',' chamlewad','Male');
+insert into student values(2,'102','mayur','budhware','Male');
+insert into student values(3,'103','Vishal','ook','Male');
+insert into student values(4,'104','swati','monit','FeMale');
+insert into student values(5,'105','Shital','sharma','FeMale');
+
+select * from student where Gender='Female';
+
+select * from student where Gender='Male';
+
+select * from student order by id desc 
+select * from student order by id asc 
+
+select * from student where rollno=101;
+select * from student where rollno=103;
+select * from student where rollno=105;
+
+select * from student where rollno in (101,103,105);
+select * from student where rollno not in (101,103,105);
+
+select max(rollno) from student
+select min(rollno) from student
+select AVG(rollno) from student
+select count(*) from student 
+
+select  * from student where FirstName like 's%'
+--name=s
+--name=sangam
+select  * from student where FirstName like '%m%'
+
+select CONCAT(FirstName,' ',LastName) as FullName  from student
 
