ec01d580d958067d699c4b281b783405ccf79de1
sonuchamlewad <csneha2620@gmail.com>
2022-06-08 16:45:42 +0530

    first commit

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -5,15 +5,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication1.Controllers
+namespace WebApplication1.Models
 {
     [Route("api/[controller]")]
     [ApiController]
     public class HomeController : ControllerBase
     {
-        public string Get()
+        public List<Student> Get()
         {
-            return "Sneha Chamlewad";
+            sampleDB1Context db = new sampleDB1Context();
+            return db.Students.ToList();
         }
     }
 }
---
+++ WebApplication1/WebApplication1/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WebApplication1.Models
+{
+    public partial class Employee
+    {
+        public int? Id { get; set; }
+        public string Empname { get; set; }
+        public string Gender { get; set; }
+    }
+}
